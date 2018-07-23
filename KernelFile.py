Kernel matrix computation bug fixing. Implemented accuracy for regression.
TODO follow step by step what's happaning inside. reimplement myLasso

KernelFile.py
@@ -28,17 +28,27 @@ class kernelWrapper:
                 self.kernelMatrix_list_.append(self._k_list[kernel_index+dataset_index*len(self.Ktype_list)].kernelMatrix(X))
         return self
     
-    def predict(self, Xts_list, weights, tr_label): #TODO make it for regression
+    def predict(self, Xts_list, weights, tr_label, Ptype = 'classification'):
         
         k_test_list = self.kernelMatrix(Xts_list).kernelMatrix_list_
         
-        pred = np.zeros(len(Xts_list[0]))
+        K_eta = sum(eta*X for eta, X in zip(weights, k_test_list))        
         
-        for k_test, w in zip(k_test_list, weights):
-            weighted_labeled_kernel = np.multiply(w*k_test.T, tr_label).T
-            pred += np.sum(weighted_labeled_kernel, axis=0)
-        pred = np.sign(pred)
-        return pred
+            
+        if Ptype == 'classification':
+            pred = np.zeros(k_test_list[0].shape)
+            for idx, row in enumerate(K_eta):
+                pred[idx, :] += np.multiply(row, tr_label)
+                
+            return np.sign(np.sum(pred, axis = 1))
+        
+        else:
+            pred = np.zeros(k_test_list[0].shape[0])
+            for idx, row in enumerate(K_eta):
+                pred[idx, :] += np.sum(np.multiply(row, tr_label))/np.sum(row)
+                
+            return pred
+        
     
     def accuracy(self, Xts_list, weights, tr_label, test_labels, test_pred = None):
         
@@ -123,23 +133,23 @@ class kernel:
 
                 self.X = self.X[:, mp.where(self_mu != 0)]
                     
-            self.K = linear_kernel(self.Xtr, X)
+            self.K = linear_kernel(X,self.Xtr)
             return  self.K
 
         if self.K_type == 'polynomial':
-            self.K = polynomial_kernel(self.Xtr, X, degree=self.param)
+            self.K = polynomial_kernel(X, self.Xtr, degree=self.param)
             return  self.K
 
         if self.K_type == 'gaussian':
-            self.K = rbf_kernel(self.Xtr, X, gamma=self.param)
+            self.K = rbf_kernel(X, self.Xtr, gamma=self.param)
             return  self.K
         
         if self.K_type == 'laplacian':
-            self.K = laplacian_kernel(self.Xtr, X, gamma=self.param)
+            self.K = laplacian_kernel(X, self.Xtr, gamma=self.param)
             return self.K
             
         if self.K_type == 'sigmoid':
-            self.K = sigmoid_kernel(self.Xtr, X, gamma=self.param)
+            self.K = sigmoid_kernel(X, self.Xtr, gamma=self.param)
             return self.K
         
     

myGridSearch.py
@@ -70,7 +70,7 @@ class myGridSearchCV:
 
                 # compute eta vector
                 #if verbose: print("\t\tComputing eta for {}".format(kw_idx))
-                eta = self.estimator.computeEta(kernelMatrix_list, IK_tr, sparsity = self.sparsity)
+                eta = self.estimator.computeEta(kernelMatrix_list, IK_tr, sparsity = self.sparsity, verbose = verbose)
 
                 # compute k_eta (approximation) for the validation set
                 kernelMatrix_list = kw_wrap.kernelMatrix(valid_list).kernelMatrix_list_
@@ -91,7 +91,7 @@ class myGridSearchCV:
         # recompute the eta for the selected configuration
         k_wrap_best = kernelWrapper(self.Xtr_list_, self.Ktype_list, self.configuration_list_[selected])
         kernelMatrix_list = k_wrap_best.kernelMatrix(Xtr_list).kernelMatrix_list_
-        eta = self.estimator.computeEta(kernelMatrix_list, self.IK_, sparsity = self.sparsity)
+        eta = self.estimator.computeEta(kernelMatrix_list, self.IK_, sparsity = self.sparsity, verbose = verbose)
         
         # sum all the kernel matrix
         k_eta = np.zeros(kernelMatrix_list[0].shape)

myLasso.py
@@ -6,13 +6,14 @@ from Utils import frobeniusInnerProduct
 
 
 class Lasso(BaseEstimator, RegressorMixin):
-    def __init__(self, alpha=1.0, max_iter=500, tol = 0.01, fit_intercept=False): #for the moment we suppose the data are centered, otherwise not works
+    def __init__(self, alpha=1.0, max_iter=500, tol = 0.01, fit_intercept=False, verbose = False): #for the moment we suppose the data are centered, otherwise not works
         self.alpha = alpha # 正則化項の係数
         self.max_iter = max_iter # 繰り返しの回数
         self.fit_intercept = fit_intercept # 切片(i.e., \beta_0)を用いるか
         self.coef_ = None # 回帰係数(i.e., \beta)保存用変数
         self.intercept_ = None # 切片保存用変数
         self.tol = tol
+        self.verbose = verbose
 
     def _soft_thresholding_operator(self, x, lambda_):
         if x > 0 and lambda_ < abs(x):
@@ -38,6 +39,7 @@ class Lasso(BaseEstimator, RegressorMixin):
                 X_eta = sum(beta_i * X_i for beta_i, X_i in zip(tmp_beta, X_list))
                 r_j = IK - X_eta#y - np.dot(X, tmp_beta)
                 
+                if self.verbose: print("r_j norm: {}".format(np.linalg.norm(r_j)))
                 if np.linalg.norm(r_j) < self.tol:
                     beta[j] = 0.0
                     break